e936a3a32fa3e5002feddfa0c93d8173516b2d38
VennamHimabindhu <vennamhimabindu78@gmail.com>
2025-09-19 21:34:37 +0530

    your message

--- app.js
+++ app.js
@@ -37,5 +37,5 @@ app.use('/api/users', require('./routes/authRoutes'));
 // ✅ Server port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
